e8c69900189455ecd8d2e776f71f385b39ba49e9
Maryery Vargas <maryeryvargas@gmail.com>
2020-04-23 10:03:24 +0100

    add bootstrap to project

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/', async function (req, res, next) {
       throw(dbData[1]);
     }
     res.render('index', {
-      title: 'Nodepop',
+      title: 'NODEPOP',
       data: dbData,
     });
   }
